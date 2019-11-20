0d288d734724457ea0b9004ef8d91c507046354a
Emmanuel Fache <emrade95@gmail.com>
2019-11-20 00:03:44 +0000

    Fixed Issue #26

--- lib/models/message.dart
+++ lib/models/message.dart
@@ -1,10 +1,7 @@
 class Message {
   int unread;
-  String name, time;
-  String content =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-
-  Message({this.name, this.time, this.unread});
+  String name, time, content;
+  Message({this.name, this.time, this.unread, this.content});
 }
 
 List<Message> messages = [
@@ -12,25 +9,30 @@ List<Message> messages = [
     name: "Smiley Store",
     time: "9:20 AM",
     unread: 5,
+    content: "Hello, please come and pick up your order. It is long overdue 😅."
   ),
   Message(
-    name: "Beauty Supply Store",
+    name: "Beauty Store",
     time: "8:10 AM",
     unread: 0,
+    content: "I did mention that i preferred the blue lipstick."
   ),
   Message(
     name: "LoveLee Bees",
     time: "Yesterday",
     unread: 0,
+    content: "Deliver the item to the location i sent to you last time. Thank you."
   ),
   Message(
     name: "FNHS Boutique",
     time: "15 Sept",
     unread: 0,
+    content: "Did you get the gift basket i sent you?"
   ),
   Message(
     name: "Anna's Corner",
     time: "11 Aug",
     unread: 0,
+    content: "Thanks for the bag. I ❤️ it"
   ),
 ];
